4f9b34a18a51f975858fd3e2d5231c7e3fa46285
Alexander Volz <Alexander.volz@nagold.de>
2020-11-22 19:54:08 +0100

    updated dockerrunscript

--- get5_eventula_apistats/dockerrunscript.sh
+++ get5_eventula_apistats/dockerrunscript.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd /get5src && git submodule update --init
-cp -rf /get5src/* /get5/
+cd /get5src && git submodule update --init --recursive
+cp -rf /get5src/get5_eventula_apistats/* /get5/
+mkdir -p /get5/scripting/get5
+mkdir -p /get5/scripting/include
+mkdir -p /get5/translations
+
+cp -rf /get5src/get5/scripting/get5/* /get5/scripting/get5/
+cp -rf /get5src/get5/scripting/include/* /get5/scripting/include/
+cp -rf /get5src/get5/translations/* /get5/translations/
+
 cd /get5
-cp -r ./dependencies/sm-json/addons/sourcemod/scripting/include/* ./addons/sourcemod/scripting/include
 smbuilder --flags='-E'
