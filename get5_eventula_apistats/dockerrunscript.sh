8875400be080523ede94c2109473ae9d351e5e8e
Alexander Volz <Alexander.volz@nagold.de>
2020-11-22 20:42:08 +0100

    fixed dockerrunscript and smbuild

--- get5_eventula_apistats/dockerrunscript.sh
+++ get5_eventula_apistats/dockerrunscript.sh
@@ -8,6 +8,10 @@ mkdir -p /get5/translations
 cp -rf /get5src/get5/scripting/get5/* /get5/scripting/get5/
 cp -rf /get5src/get5/scripting/include/* /get5/scripting/include/
 cp -rf /get5src/get5/translations/* /get5/translations/
+cp /get5src/LICENSE /get5/
+cp /get5src/README.md /get5/
+
+cp -r /get5src/get5/dependencies/sm-json/addons/sourcemod/scripting/include/* /get5/addons/sourcemod/scripting/include
 
 cd /get5
 smbuilder --flags='-E'
